3cd103f50517d0d5889d693d71034efd62c1d769
mcosta0131 <34768777+mcosta0131@users.noreply.github.com>
2024-04-20 09:36:17 -0300

    Alterações

--- src/main/java/com/fiap/axur/fiapaxurwhatsappsender/controller/WhatsAppAlertController.java
+++ src/main/java/com/fiap/axur/fiapaxurwhatsappsender/controller/WhatsAppAlertController.java
@@ -1,6 +1,8 @@
 package com.fiap.axur.fiapaxurwhatsappsender.controller;
 
 import com.fiap.axur.fiapaxurwhatsappsender.model.MensagemDTO;
+import com.fiap.axur.fiapaxurwhatsappsender.service.WhatsAppSenderService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WhatsAppAlertController {
 
+    @Autowired
+    private WhatsAppSenderService whatsAppSenderService;
+
     @PostMapping("/alert")
     public void sendAlert(@RequestBody MensagemDTO mensagemDTO) {
-        System.out.println(mensagemDTO.getNome());
+        whatsAppSenderService.sendMessage(mensagemDTO);
     }
 }
